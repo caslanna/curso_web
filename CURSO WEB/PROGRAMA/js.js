6c3f61a74c6539b97b81104034af1eecf8e9199d
caslanna <93556869+caslanna@users.noreply.github.com>
2021-11-01 20:14:26 -0300

    Primeiro commit

---
+++ CURSO WEB/PROGRAMA/js.js
@@ -0,0 +1,25 @@
+console.log('mayk')
+console.log(33)
+console.log(Infinity)
+console.log(12-6)
+
+/*
+{propriedade:"valor"}
+*/
+
+let dados = {
+    nome: "seu nome",
+    idade: 34,
+    endereco:" 704 norte"
+}
+console.log({chave:"texto qualquer"})
+
+console.log({
+    nome: "Mayk",
+    idade: 30,
+    a:dados
+
+})
+console.log(dados)
+
+console.log(["mark", 36])
---
+++ CURSO WEB/PROGRAMA/scope.md
@@ -0,0 +1,9 @@
+# scope
+
+// var é global e também local
+
+{
+    var x = 0
+
+}
+ 
